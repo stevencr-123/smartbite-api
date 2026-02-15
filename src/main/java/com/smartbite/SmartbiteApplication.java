5e6a8e47b677caa500977f6f49d24cb49f554918
stevencr-123 <caraballostevenson5@gmail.com>
2026-02-15 08:45:18 -0500

    feat: initial Spring Boot project structure

---
+++ src/main/java/com/smartbite/SmartbiteApplication.java
@@ -0,0 +1,13 @@
+package com.smartbite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartbiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartbiteApplication.class, args);
+	}
+
+}
